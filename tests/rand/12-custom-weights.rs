6f09fc63ac5a657e0e04f7c77654e993c356fe3f
Matthew Taylor <mwtaylor902@gmail.com>
2023-02-27 23:00:39 -0500

    Added test case for zero weight

--- tests/rand/12-custom-weights.rs
+++ tests/rand/12-custom-weights.rs
@@ -4,7 +4,10 @@ use enum_derived::Rand;
 
 #[derive(Rand)]
 pub enum RandomWeights {
+    // Default weight is one
     Default,
+    #[weight(0)]
+    Zero,
     #[weight(1)]
     One,
     #[weight(2)]
@@ -23,6 +26,9 @@ fn main() {
         let counts = seen_variants.entry(std::mem::discriminant(&rt)).or_default();
         *counts += 1;
     }
+
+    let zero_count = seen_variants.get(&std::mem::discriminant(&RandomWeights::Zero));
+    assert!(zero_count.is_none());
 
     let default_count = seen_variants.get(&std::mem::discriminant(&RandomWeights::Default)).unwrap();
     let one_count = seen_variants.get(&std::mem::discriminant(&RandomWeights::One)).unwrap();
